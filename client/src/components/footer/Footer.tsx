a00756e8cdc907cfc401058dd9dd4b18727ba77d
lvbn <luiz.neto.v@gmail.com>
2023-07-05 23:18:37 -0300

    [FE][Footer][clearing warnings]

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,8 +1,6 @@
 
 import styles from './Footer.module.css'
 
-import { Link } from 'react-router-dom';
-
 export default function Footer() {
   return (
     <footer className={styles.footer_container}>
